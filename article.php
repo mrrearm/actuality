84af7fb253f06f1ea27e749f2173e8eb13b34fc4
mrrearm <mrrearm@libero.it>
2026-08-27 03:43:26 +0200

    Fix: link di condivisione generati in https dietro proxy (Render)

--- article.php
+++ article.php
@@ -21,7 +21,9 @@ $comments = get_comments($pdo, $id, true);
 
 $hasVoted = isset($_COOKIE['voted_articles']) && in_array((string)$id, explode(',', $_COOKIE['voted_articles']), true);
 
-$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$forwardedProto = strtolower($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '');
+$isHttps = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $forwardedProto === 'https';
+$scheme = $isHttps ? 'https' : 'http';
 $currentUrl = $scheme . '://' . ($_SERVER['HTTP_HOST'] ?? '') . url('article.php?id=' . $id);
 $shareTitle = rawurlencode($article['title']);
 $shareUrl = rawurlencode($currentUrl);
